a5644e92a3acaecbcad82db05420fc645fcd33c1
Palash-it <palash.debnath5@gmail.com>
2020-02-15 03:06:13 +0600

    removed unused import

--- src/test/java/com/devsoftbd/personrestapi/repository/PersonRepositoryIntegrationTest.java
+++ src/test/java/com/devsoftbd/personrestapi/repository/PersonRepositoryIntegrationTest.java
@@ -12,7 +12,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.devsoftbd.personrestapi.model.HobbyModel;
