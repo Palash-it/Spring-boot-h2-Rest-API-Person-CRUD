196f07875352b9b5c73329ca5e739ceb62a9926e
Palash-it <palash.debnath5@gmail.com>
2020-02-15 11:52:11 +0600

    POM updated to build war to tomcat, added tomcat required config on springboot main class and added port 8080 to properties file

--- src/main/java/com/devsoftbd/personrestapi/PersonRestApiApplication.java
+++ src/main/java/com/devsoftbd/personrestapi/PersonRestApiApplication.java
@@ -2,12 +2,13 @@ package com.devsoftbd.personrestapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class PersonRestApiApplication {
+public class PersonRestApiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PersonRestApiApplication.class, args);
